8997d984e54c22e6d2eb886de98bc5c23e196a2c
andreasholmsen <logins@andreash.no>
2025-04-29 18:17:40 +0200

    Fin de séance

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -1,5 +1,13 @@
 package org.insa.graphs.algorithm.shortestpath;
 
+import java.nio.file.Path;
+import java.util.PriorityQueue;
+
+import org.insa.graphs.algorithm.utils.BinaryHeap;
+import org.insa.graphs.model.Arc;
+import org.insa.graphs.model.Graph;
+import org.insa.graphs.model.Node;
+
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
     public DijkstraAlgorithm(ShortestPathData data) {
@@ -16,7 +24,21 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
         // variable that will contain the solution of the shortest path problem
         ShortestPathSolution solution = null;
 
-        // TODO: implement the Dijkstra algorithm
+        Graph graph = data.getGraph();
+        BinaryHeap<Label> Distances = new BinaryHeap<>();
+
+        Label pointer = null;
+        //Init de tableau
+        for (Node node : graph.getNodes()) {
+            if (node.equals(data.getOrigin())) {
+                pointer = new Label(node, true, 0, null);
+                Distances.insert(pointer);
+            } else {
+                Distances.insert(new Label(node, false, Float.MAX_VALUE, null));
+            }
+        }
+
+
 
         // when the algorithm terminates, return the solution that has been found
         return solution;
